aa60e22a6275ee6c5dfaa3cbccd478403fb98bea
Jonel Lenteria <jlenteria98@gmail.com>
2022-02-03 15:01:44 +0800

    cleanup

--- assets/scripts/admin/index.js
+++ assets/scripts/admin/index.js
@@ -2,7 +2,7 @@
   "use strict";
   var App = angular.module("Plugins").config(function($stateProvider) {
     $stateProvider.state("plugins.mini_browser", {
-      templateUrl: "/public/plugins/mini-browser-plugin/views/admin/index.html",
+      templateUrl: "/public/plugins/mini-browser/views/admin/index.html",
       controller: "MiniBrowserPluginCtrl",
       url: "/mini-browser-plugin",
       title: "Mini Browser"
